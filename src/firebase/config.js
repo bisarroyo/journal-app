efd5ca421e646ca78f8e616e01a41e600f632dea
Bismark Arroyo <bis.arroyo@gmail.com>
2023-11-15 10:57:28 -0600

    add firebase package, change project nm

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAz7_V6AH4kuOMI3t7bJ9J7Vp8WhwmGKzQ',
+  authDomain: 'react-firebase-1a7b5.firebaseapp.com',
+  projectId: 'react-firebase-1a7b5',
+  storageBucket: 'react-firebase-1a7b5.appspot.com',
+  messagingSenderId: '547700854572',
+  appId: '1:547700854572:web:96dee3df9b67b7e3ffe689',
+  measurementId: 'G-SQYPCBSTF3'
+}
+
+// Initialize Firebase
+export const FirebaseApp = initializeApp(firebaseConfig)
+export const FirebaseAuth = getAuth(FirebaseApp)
