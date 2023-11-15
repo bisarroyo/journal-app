06d5cd3f7ad04a4493ce6807cb647e21cc5c7630
Bismark Arroyo <bis.arroyo@gmail.com>
2023-11-15 11:04:58 -0600

    add config to cloud db firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore/lite'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyAz7_V6AH4kuOMI3t7bJ9J7Vp8WhwmGKzQ',
@@ -15,3 +16,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig)
 export const FirebaseAuth = getAuth(FirebaseApp)
+export const FirebaseDB = getFirestore(FirebaseApp)
