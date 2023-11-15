1ac86222213f914856aace45dd51c78a5a550914
Bismark Arroyo <bis.arroyo@gmail.com>
2023-11-15 14:22:25 -0600

    add auth logic for firebase

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,16 +3,33 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    status: 'checking', // non-authenticated | authenticated
+    status: 'checking', // 'checking', 'not-authenticated', 'authenticated'
     uid: null,
     email: null,
     displayName: null,
-    photoUrl: null
+    photoURL: null,
+    errorMessage: null
   },
   reducers: {
-    login: (state, action) => {},
-    logout: (state, action) => {},
-    checkingCredentials: (state, action) => {}
+    login: (state, { payload }) => {
+      state.status = 'authenticated' // 'checking', 'not-authenticated', 'authenticated'
+      state.uid = payload.uid
+      state.email = payload.email
+      state.displayName = payload.displayName
+      state.photoURL = payload.photoURL
+      state.errorMessage = null
+    },
+    logout: (state, { payload }) => {
+      state.status = 'not-authenticated' // 'checking', 'not-authenticated', 'authenticated'
+      state.uid = null
+      state.email = null
+      state.displayName = null
+      state.photoURL = null
+      state.errorMessage = payload?.errorMessage
+    },
+    checkingCredentials: (state) => {
+      state.status = 'checking'
+    }
   }
 })
 
